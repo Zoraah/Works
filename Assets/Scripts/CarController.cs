374da58fac4dd66539621386a9a8875b29a8523f
Volodymyr Chernyavskyy <vovikche@gmail.com>
2021-09-26 20:38:27 +0300

    added some types of road added automatical camera-control for car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public Camera camera;
+
     private Rigidbody rb;
     [SerializeField]
     private float MoveSpeed;
@@ -29,6 +31,10 @@ public class CarController : MonoBehaviour
     {
         
     }
+    void LateUpdate()
+    {
+        CameraControl();
+    }
     private void Move()
     {
         Vector3 move = transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
@@ -38,5 +44,22 @@ public class CarController : MonoBehaviour
     {
         Quaternion turn = Quaternion.Euler(0f, TurnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f);
         rb.MoveRotation(rb.rotation*turn);
+        
+    }
+    private void CameraControl()
+    {
+        camera.gameObject.transform.rotation = Quaternion.Euler(60f, -60f, 0f);
+        camera.gameObject.transform.position = new Vector3(transform.position.x + 12f, transform.position.y + 26f, transform.position.z - 7.5f);
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0 && camera.orthographicSize < (5 + Mathf.Abs(Input.GetAxis("Vertical")) * 5))
+        {
+            camera.orthographicSize += 0.01f;
+        }
+        else
+        {
+            if(camera.orthographicSize>5)
+            {
+                camera.orthographicSize -= 0.01f;
+            }
+        }
     }
 }
